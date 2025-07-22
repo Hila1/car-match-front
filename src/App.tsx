97bb877c571c1a8702b48c9ff6cd97a19f6bed5a
Hila Shapira <hilag1997@gmail.com>
2025-07-22 15:14:25 +0300

    Merge i18n setup and string centralization into master
    Merging the i18n refactor with react-i18next and centralized strings into master branch.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AdBanner from './components/AdBanner/AdBanner';
+import SearchForm from './components/SearchForm/SearchForm';
+import SearchHistory from './components/SearchHistory/SearchHistory';
+import CarList from './components/CarList/CarList';
+import { Car } from './components/CarCard/CarCard';
+import Results from './pages/Results';
+import { Routes, Route } from 'react-router-dom';
+
+const mockCars: Car[] = [
+  {
+    model: 'Toyota Corolla',
+    image: 'https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_1280.jpg',
+    specs: '1.8L, Automatic, 5 seats',
+    manufacturerUrl: 'https://www.toyota.com/corolla/',
+  },
+  {
+    model: 'Honda Civic',
+    image: 'https://cdn.pixabay.com/photo/2016/11/29/09/32/auto-1868726_1280.jpg',
+    specs: '2.0L, Manual, 5 seats',
+    manufacturerUrl: 'https://automobiles.honda.com/civic-sedan',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AdBanner />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <SearchForm />
+            <SearchHistory />
+            <CarList cars={mockCars} />
+          </>
+        } />
+        <Route path="/results" element={<Results />} />
+      </Routes>
     </div>
   );
 }
